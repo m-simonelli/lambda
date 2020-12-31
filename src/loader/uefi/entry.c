6b2b2dcc6401cfb67d424028c45286bd73e5a68c
Marco Simonelli <msimonelli@protonmail.com>
2020-12-31 16:06:12 +1100

    uefi hello world

--- src/loader/uefi/entry.c
+++ src/loader/uefi/entry.c
@@ -1,1 +1,10 @@
-/* todo */
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS
+EFIAPI
+efi_main(EFI_HANDLE im_hdl, EFI_SYSTEM_TABLE *sys_tbl) {
+    InitializeLib(im_hdl, sys_tbl);
+    Print(L"UEFI loaded!\n");
+    return EFI_SUCCESS;
+}
