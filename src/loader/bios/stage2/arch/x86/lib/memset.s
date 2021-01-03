223b676d45fb6ee9040fc8139de238b3a37d0626
Marco Simonelli <msimonelli@protonmail.com>
2021-01-03 13:16:24 +1100

    minor memset optimizations + fixed params

--- src/loader/bios/stage2/arch/x86/lib/memset.s
+++ src/loader/bios/stage2/arch/x86/lib/memset.s
@@ -5,21 +5,23 @@
 ; terms of the GPL version 3 (see the file LICENSE)
 
 memset:
-    pop eax ; dst
-    pop edx ; val
-    pop ecx ; count
-
     push ebp
-    push ebx
+    mov ebp, esp
+
+    mov eax, [ebp + 8]  ; dst
+    mov edx, [ebp + 12] ; val
+    mov ecx, [ebp + 16] ; count
+
     push edi
     push esi
+    mov esi, eax
 
     ; count == 0
     test ecx, ecx
     jz .done 
 
     ; align dst to 4 byte boundary
-    test ecx, 3
+    test eax, 3
     jnz .fix_align
 
   .align_fixed:
@@ -34,6 +36,7 @@ memset:
 
     align 16
   .loop_32bytes:
+    sub ecx, 32
     mov DWORD [eax],      edx
     mov DWORD [eax + 4],  edx
     mov DWORD [eax + 8],  edx
@@ -42,30 +45,34 @@ memset:
     mov DWORD [eax + 20], edx
     mov DWORD [eax + 24], edx
     mov DWORD [eax + 28], edx
-    sub ecx, 32
+    lea eax, [eax + 32]
     jae .loop_32bytes
+    lea ecx, [ecx + 32]
 
     align 16
   .loop_1byte:
-    mov BYTE [eax], dl
     sub ecx, 1
+    mov BYTE [eax], dl
+    lea eax, [eax + 1]
     jae .loop_1byte
+    lea ecx, [ecx + 1]
 
     align 16
   .done:
     pop esi
     pop edi
-    pop ebx
     pop ebp
 
+    mov eax, esi ; rval
     ret
 
     align 16
   .fix_align:
     ; align dst to be 4 byte aligned
-    mov BYTE [eax], dl
     lea ecx, [ecx - 1]
-    test ecx, 3
+    mov BYTE [eax], dl
+    lea eax, [eax - 1]
+    test eax, 3
     jnz .fix_align
     jmp .align_fixed
     
